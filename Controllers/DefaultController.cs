c91bcee1bf369673efd8c86a25183bb7faecc7c2
Sahil Rzayev <sahilrzayev200d@gmail.com>
2022-12-02 01:08:42 +0300

    Add project files.

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UowDesignPattern.DataAccessLayer.Concrete;
+using UowDesignPattern.EntityLayer.Concrete;
+
+namespace UowDesignPattern.PresentationLayer.Controllers
+{
+    public class DefaultController : Controller
+    {
+        Context context = new Context();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            context.Update(new Customer
+            {
+                CustomerID = 2,
+                Name = "Mehmet Ali",
+                Surname = "Kurtulmuş",
+                Balance = 10500
+            });
+            context.SaveChanges();
+
+            context.Update(new Customer
+            {
+                CustomerID = 3,
+                Name = "Ayşenur",
+                Surname = "Çuhacı",
+                Balance = 11500
+            });
+            context.SaveChanges();
+            return View();
+        }
+    }
+}
